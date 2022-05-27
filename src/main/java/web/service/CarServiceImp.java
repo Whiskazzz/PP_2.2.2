a12b370a83a00cd5bf371e989e66cf952821e62e
Whiskazzz <wilker2010@yandex.ru>
2022-05-28 01:10:17 +0300

    1.1 add logic to num>5

--- src/main/java/web/service/CarServiceImp.java
+++ src/main/java/web/service/CarServiceImp.java
@@ -8,20 +8,23 @@ import java.util.List;
 public class CarServiceImp implements CarService {
     private List<Car> createCars() {
         List<Car> cars = new ArrayList<>(5);
-        cars.add(new Car(123,1000,"koyota"));
-        cars.add(new Car(124,1500,"gada"));
-        cars.add(new Car(125,500,"many cooper"));
-        cars.add(new Car(126,1500,"supraaa"));
-        cars.add(new Car(127,1000,"nesupra"));
+        cars.add(new Car(123, 1000, "koyota"));
+        cars.add(new Car(124, 1500, "gada"));
+        cars.add(new Car(125, 500, "many cooper"));
+        cars.add(new Car(126, 1500, "supraaa"));
+        cars.add(new Car(127, 1000, "nesupra"));
         return cars;
     }
 
     @Override
     public List<Car> getNumOfCars(Integer num) {
         if (num != null) {
-            return createCars().subList(0,num);
-        }
-        else {
+            if (num <= 5) {
+                return createCars().subList(0, num);
+            } else {
+                return createCars();
+            }
+        } else {
             return createCars();
         }
     }
